03d693721fb46b35204af0a5497070b63990bb00
vboharpi <vrushabh.boharpi@niceactimize.com>
2020-07-13 11:02:21 +0530

    First Commit

---
+++ src/main/java/com/nice/presurveilx/PreSurveilxApplication.java
@@ -0,0 +1,13 @@
+package com.nice.presurveilx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreSurveilxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreSurveilxApplication.class, args);
+	}
+
+}
